dc4da6b6f076985a2fcaed2b385bf990025a15e0
iLQ <danielcardeenas@gmail.com>
2015-02-18 05:18:07 +0000

    Borders detection completed.

--- Borders/main.cpp
+++ Borders/main.cpp
@@ -1,49 +1,67 @@
 #include <iostream>
 #include <chrono>
+#include <numeric>
 
 #include "opencv2/opencv.hpp"
 #include "Kernel.h"
 #include "Filters.h"
+#include "Utils.h"
 
 using namespace std;
 using namespace cv;
-    
+
+void TimeFunc( void (*f)(int) );
+
 int main(int argc, char** argv)
 {
-     Mat img, out_img;
+    Mat colorImg = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
      
-     //img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-     img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-     out_img = Mat::zeros(img.size(), img.type());
+    Mat joinImg = Mat::zeros(img.size(), img.type());
+    Mat imgAbs = Mat::zeros(img.size(), img.type());
+    Mat gradY = Mat::zeros(img.size(), img.type());
+    Mat gradX = Mat::zeros(img.size(), img.type());
+    Mat final = Mat::zeros(img.size(), img.type());
 
-    // Sobel kernel
-    Kernel gaus ({
-        {1,2,1},
-        {2,4,2},
-        {1,2,1}
+    /// Supports n*n size
+    /// As long it's odd sized and squared (same size for the 2 dimensions)
+    /// Kernel.cpp for more info
+    Kernel soby ({
+            {1,2,1},
+            {0,0,0},
+            {-1,-2,-1}
     });
 
-    Kernel sob ({
+    Kernel sobx ({
             {-1,0,1},
             {-2,0,2},
             {-1,0,1}
     });
-
-    //cvtColor(img,img,CV_RGB2GRAY);
     
-    cout << img.channels() << endl;
-
-    // Start counting time
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    /// Filters.cpp for more info
+    Convolution(img, gradX, sobx);
+    Convolution(img, gradY, soby);
     
-    // Where magic happens
-    Convolution(img, out_img, sob);
+    /// Filters.cpp for more info
+    EuclidianJoin(gradX, gradY, joinImg);
+    // AbsJoin(gradX, gradY, joinImg);
     
-    // Stop counting and print time elapsed
-    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
-    std::cout << "Time elapsed fast: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " Miliseconds" <<std::endl;
+    /// Filters.cpp  for more info
+    DeviationThreshold(joinImg, final);
+    // LazyThreshold(out_img, final);
+    // MeanThreshold(out_img, finalmean);
     
-    // Write image in same directory
-    std::cout << "Writing image... (" << "sobel.jpg" << ")" << std::endl;
-    imwrite("sobel.jpg", out_img);
+    imwrite("binarized.jpg", final);
+    
+    // Set a border marker
+    cv::Vec3b marker;
+    marker[0] = 0;
+    marker[1] = 0;
+    marker[2] = 255;
+    
+    SpecialReplacePixels(colorImg, final, marker);
+    imwrite("final.jpg", colorImg);
+    
+    /// TODO:
+    /// Change borders color
 }
